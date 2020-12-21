e61461a003ec28ab4d9cad800738b4258f1ce778
Seveshy <danielalvesmajor@outlook.com>
2020-12-21 18:00:14 -0300

    Custom theme

---
+++ admin/src/containers/AuthPage/components/Logo/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import LogoWonDark from '../../../../assets/images/logo-won-dark.svg';
+import Img from './Img';
+
+const Logo = () => <Img src={LogoWonDark} alt="Logo Won Games" />;
+
+export default Logo;
